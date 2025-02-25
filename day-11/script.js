fdde108fdd3567b13355c9bbf568b39cbb22da0f
aman8673 <amankumart8673@gmail.com>
2025-02-25 15:47:02 +0530

    hello

---
+++ day-11/script.js
@@ -0,0 +1,70 @@
+// // let obj = {
+// //     name: "AMAN SINGH",
+// //     class: "B.tech CSE",
+// //     myfunction:function(){
+// //         console.log(this);
+        
+// //     console.log("I am a method inside object");
+    
+// //     },
+// //     subjects:["DBMS","CN","ALML","CA","NM"],
+// //     newObj:{
+// //         1:80,
+// //         nestedFunction:function(){
+// //             console.log(this);
+
+// //             console.log("I am a nested Function");
+
+// //         },
+// //     },
+// // };
+
+// // obj.myfunction()
+// // obj.newObj.nestedFunction();
+
+// // let num = 9;
+
+
+// // if (num % 3 == 0 && num % 5 == 0) {
+// //   console.log("FizzBuzz");
+// // } else if (num % 5 == 0) {
+// //   console.log("Buzz");
+// // } else if (num % 3 == 0) {
+// //   console.log("Fizz");
+// // } else {
+// //   console.log("wrong number");
+// // }
+
+
+
+// // for loop><
+// let str ="shanti"
+
+// for (let i = 0; i< str; i++) {
+//     console.log("a",str[i]);
+    
+// }
+
+// let num =5
+// for (let i=0; i<num; i++){
+//     console.log(i)
+// }
+
+
+// let arr = ["a","b","c","d","e","f","g","h"]
+// for (const alph of arr){
+//     console.log(alph);
+// }
+
+
+let  numbers =[1,2,3,4,5,6,7,8,9,10];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i] + " is Even");
+    } else {
+        console.log(numbers[i] + " is Odd");
+    }
+}
+
+
---
+++ day-9/classwork/index.js
@@ -0,0 +1,16 @@
+myName()
+
+
+
+
+const myFunction=(a,b)=>{
+    console.log(a+b)
+function myNam(){
+
+
+    / let a=10;
+// let b=10;
+
+
+
+// myFunction(a,b)
